5ed4177e64c5d29fdb8e123d014056fbe0c70f47
Anton Kudryashov <driver_90@list.ru>
2025-05-09 12:38:58 +0700

    решенная задача 7.4

--- 7.3/jest/app.js
+++ 7.3/jest/app.js
@@ -5,7 +5,8 @@ function sortByName(inputArray) {
     if (nameA < nameB)
       //сортируем названия по возрастанию
       return -1;
-    if (nameA > nameB) return 1;
+    if (nameA > nameB) 
+      return 1;
     return 0; // нет сортировки
   });
   return inputArray;
--- 7.3/jest/test/unit/sortByName.test.js
+++ 7.3/jest/test/unit/sortByName.test.js
@@ -1,17 +1,51 @@
 const sorting = require("../../app");
 
 describe("Books names test suit", () => {
-  it("Books names should be sorted in ascending order", () => {
-    expect(
-      sorting.sortByName([
-        "Гарри Поттер",
-        "Властелин Колец",
-        "Волшебник изумрудного города",
-      ])
-    ).toEqual([
+   it("Books names should be sorted in ascending order", () => {
+    const input = ["Гарри Поттер",
+         "Властелин Колец",
+         "Волшебник изумрудного города",];
+
+    const expected = [
       "Властелин Колец",
       "Волшебник изумрудного города",
+      "Гарри Поттер",];
+
+    const output = sorting.sortByName(input);
+
+    expect(output).toEqual(expected);
+   });
+
+
+   it("Test2", () => {
+    const input = [
+      "F", "F",];
+
+    const expected = [
+      "F", "F",];
+
+    const output = sorting.sortByName(input);
+
+    expect(output).toEqual(expected);
+   });
+
+   it("Test3", () => {
+    const input = [
+      "Волшебник изумрудного города",
+      "Властелин Колец",
       "Гарри Поттер",
-    ]);
-  });
+      ];
+
+    const expected = [
+      "Властелин Колец",
+      "Волшебник изумрудного города",
+      "Гарри Поттер",];
+
+    const output = sorting.sortByName(input);
+
+    expect(output).toEqual(expected);
+   });
+
 });
+
+
